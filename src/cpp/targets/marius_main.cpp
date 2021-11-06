e56bb6e1d3fe4b04c477b214c57a28fdea5ff206
rachit173 <rachittibrewal@outlook.com>
2021-11-06 23:56:39 +0000

    Reorganizing marius.cpp to separate driver code from library code, build works now.

---
+++ src/cpp/targets/marius_main.cpp
@@ -0,0 +1,16 @@
+
+#include "marius.h"
+
+#include "config.h"
+#include "evaluator.h"
+#include "io.h"
+#include "logger.h"
+#include "model.h"
+#include "trainer.h"
+#include "util.h"
+
+using std::get;
+
+int main(int argc, char *argv[]) {
+    marius(argc, argv);
+}
